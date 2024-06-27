4ebe6c0e0aa0c0779fd4c220e0d2f3f1d0d76c0b
CJ Brewer <brewercalvinj@gmail.com>
2024-06-27 10:12:15 -0600

    Initial commit

---
+++ test/cipherstash-cts-aws-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CipherstashCtsAwsCdk from '../lib/cipherstash-cts-aws-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cipherstash-cts-aws-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CipherstashCtsAwsCdk.CipherstashCtsAwsCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
